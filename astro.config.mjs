860540251cfe0cc80c56f6892a5252755dff5a5e
willyw.dev <kontakt@willyw.dev>
2024-03-22 18:45:57 +0100

    add astro-icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@ import { defineConfig } from "astro/config";
 import { astroImageTools } from "astro-imagetools";
 import tailwind from "@astrojs/tailwind";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), astroImageTools],
+  integrations: [tailwind(), astroImageTools, icon()]
 });
