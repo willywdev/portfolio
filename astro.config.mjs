717d02838b84af82ab54f679e63f047ad271dcd5
willyw.dev <kontakt@willyw.dev>
2024-03-22 19:44:00 +0100

    add vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,5 +9,7 @@ import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
   integrations: [tailwind(), astroImageTools, icon(), react()],
   output: "server",
-  adapter: vercel(),
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
 });
