9a08cc9f93a97b5ae69724e8cfdc09450310e743
willyw.dev <kontakt@willyw.dev>
2024-03-22 18:48:18 +0100

    add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import { astroImageTools } from "astro-imagetools";
 import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), astroImageTools, icon()]
+  integrations: [tailwind(), astroImageTools, icon(), react()]
 });
