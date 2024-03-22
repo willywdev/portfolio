42335cb6b5757dd533fd9856277b1ff0e9c4f499
willyw.dev <kontakt@willyw.dev>
2024-03-22 18:49:03 +0100

    add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@ import { defineConfig } from "astro/config";
 import { astroImageTools } from "astro-imagetools";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-
 import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), astroImageTools, icon(), react()]
+  integrations: [tailwind(), astroImageTools, icon(), react()],
+  output: "server",
+  adapter: vercel()
 });
