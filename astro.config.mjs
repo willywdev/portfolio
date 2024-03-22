6496e2e3a9237b28c11f453c5954268dac889c10
willyw.dev <kontakt@willyw.dev>
2024-03-22 18:56:03 +0100

    add biome

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,13 @@ import { astroImageTools } from "astro-imagetools";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import react from "@astrojs/react";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import biome from "astro-biome";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), astroImageTools, icon(), react()],
+  integrations: [tailwind(), astroImageTools, icon(), react(), biome()],
   output: "server",
   adapter: vercel()
 });
