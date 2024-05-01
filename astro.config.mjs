24f59d8b3f99ec65f6bdf4d8b0bb47f44c2d8770
willyw.dev <kontakt@willyw.dev>
2024-05-01 14:30:14 +0200

    add sitemap and robots generator

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,32 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-import react from "@astrojs/react";
-import million from "million/compiler";
 import vercel from "@astrojs/vercel/serverless";
 import playformCompress from "@playform/compress";
+import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
+
+/**
+ * Remove Million and React if no React Components are used
+ * import react from "@astrojs/react";
+ * import million from "million/compiler";
+ */
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [million.vite({
-    mode: "react",
-    server: true
-    // compress needs to be last always
-  }), tailwind(), icon(), react(), playformCompress()],
-  output: "server",
+  integrations: [
+  //million.vite({
+  //mode: "react",
+  //server: true
+  // compress needs to be last always
+  // }),
+  tailwind(), icon(),
+  // react(),
+  sitemap(), robotsTxt(), playformCompress()],
+  output: "hybrid",
   adapter: vercel()
+  /**
+   * Activate this when this is switched to main domain 
+   * site: "https://willyw.dev",
+   */
 });
