bdc2328938415e2307d7a083ca78330fca5d1377
willyw.dev <kontakt@willyw.dev>
2024-05-01 12:54:01 +0200

    add more dependencies, edit front page

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,17 @@ import icon from "astro-icon";
 import react from "@astrojs/react";
 import million from "million/compiler";
 import vercel from "@astrojs/vercel/serverless";
+import astroI18Next from "astro-i18next";
+
+import playformCompress from "@playform/compress";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [million.vite({
     mode: "react",
     server: true
-  }), tailwind(), icon(), react()],
+    // compress needs to be last always
+  }), tailwind(), icon(), astroI18Next(), react(), playformCompress()],
   output: "server",
   adapter: vercel()
 });
