4c0e5774fc476adbd7d10da69312650215b2592a
willyw.dev <kontakt@willyw.dev>
2024-05-01 09:56:44 +0200

    replace screen for dvh

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,6 +1,6 @@
 export default function DotBackgroundDemo() {
 	return (
-		<div className="pointer-events-none top-0 left-0 h-screen w-screen dark:bg-black bg-white  dark:bg-grid-small-white/[0.2] bg-grid-small-black/[0.2] fixed inset-0 z-[-1]">
+		<div className="pointer-events-none top-0 left-0 h-dvh w-dvw dark:bg-black bg-white  dark:bg-grid-small-white/[0.2] bg-grid-small-black/[0.2] fixed inset-0 z-[-1]">
 			<div className="absolute inset-0 dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
 		</div>
 	);
